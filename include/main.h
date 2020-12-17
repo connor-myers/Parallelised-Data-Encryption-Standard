6f73e7d6d508ed160c47fc5b48f601e5e8ec3c08
Connor Myers <myers.connor.james@gmail.com>
2020-12-17 16:03:21 +1000

    some mpi code

--- include/main.h
+++ include/main.h
@@ -1,6 +1,45 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
 #include <getopt.h>
+
+#include "mpi.h"
+
+/**
+ * enum: des_mode
+ * ----------------------
+ * Different possible modes for des - encryption or decryption
+ */
+enum des_mode {
+        INVALID,
+        ENCRYPTION,
+        DECRYPTION
+};
+
+/**
+ * struct: user_settings
+ * ----------------------
+ * Contains parsed user-settings from command-line arguments
+ * mode:        Represents whether user is encrypting (1) or decryption (2).
+ * key:         The path to the .key file containing key. NULL if not specified.
+ * target:      The path to the file to be encrypted or decrypted
+ */
+struct user_settings
+{
+        enum des_mode   mode;
+        char            *key;
+        char            *target;
+};
+
+/**
+ * function: user_settings
+ * -----------------------
+ * Loads command-line arguments into user settings struct
+ */
+void load_settings(struct user_settings *settings, int numArgs, char **args);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,5 +3,48 @@
 int
 main(int argc, char **argv)
 {
+        MPI_Init(&argc, &argv);
+
+        // initialising MPI variables 
+        int worldSize;
+        int numWorkers;
+        int myRank;
+        MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+        MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+        numWorkers = worldSize - 1;
+
+        if (myRank == 0) {
+                // passing command-line arguments 
+                struct user_settings settings;
+                load_settings(&settings, argc - 1, argv + 1);
+        }
+
+        MPI_Finalize(); 
+        
         return 0;
+}
+
+/**
+/**
+ * function: user_settings
+ * -----------------------
+ * Loads command-line arguments into user settings struct
+ * 
+ * settings:    pointer to empty user_settings struct. Will get filled with
+ *              corresponding arguments from commandline arguments
+ * numArgs:     Number of command-line arguments (not including program name) 
+ * args:        Command-line arguments (not including program name) 
+ */
+void
+load_settings(struct user_settings *settings, int numArgs, char **args)
+{
+        // default values
+        memset(settings, 0, sizeof(struct user_settings));
+        
+        // int c;
+        // while((c = getopt(numArgs, args, "edk:")) != -1) {
+        //         switch (c) {
+
+        //         }
+        // }                       
 }
